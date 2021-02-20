145c6cc1fb872b947732546ede9b1ec179378157
pen <petypen@yandex.ru>
2021-02-20 16:07:08 +0300

    feature: начальный коммит
    Пустой шаблон консольного приложения

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace another_backup_of_1C_database
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

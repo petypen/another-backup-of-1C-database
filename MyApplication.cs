d49a1a0858b025460fc2b8402cdd3f072dbfc001
pen <petypen@yandex.ru>
2021-02-22 22:19:21 +0300

    feature: встроено логирование
    -- информация получена из
    https://www.thecodebuzz.com/logging-in-net-core-console-application/

---
+++ MyApplication.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+
+namespace another_backup_of_1C_database
+{
+    public class MyApplication
+    {
+        private readonly ILogger logger;
+        public MyApplication(ILogger<MyApplication> logger)
+        {
+            this.logger = logger;
+        }
+        internal void Run()
+        {
+            logger.LogInformation("Приложение запущено {dateTime}", DateTime.Now);
+            Console.WriteLine("Привет мир");
+            logger.LogWarning("Приложение  остановлено");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
 
 namespace another_backup_of_1C_database
 {
@@ -6,7 +8,17 @@ namespace another_backup_of_1C_database
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            using ServiceProvider serviceProvider = services.BuildServiceProvider();
+            MyApplication app = serviceProvider.GetService<MyApplication>();
+
+            app.Run();
+        }
+
+        private static void ConfigureServices(ServiceCollection services)
+        {
+            services.AddLogging(configure => configure.AddConsole()).AddTransient<MyApplication>();
         }
     }
 }
